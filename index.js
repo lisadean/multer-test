55172672fd90f74acc3aebb42d1287e36a59e38e
Lisa Dean <lisa@lisadean.net>
2018-10-08 11:32:13 -0400

    test multer code

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+var multer  = require('multer')
+var storage = multer.diskStorage({
+  destination: 'uploads/',
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+var upload = multer({ storage: storage })
+const app = express();
+
+const static = express.static;
+app.use(static('./public/'));
+
+app.post('/picture', upload.single('imagefile'), function (req, res, next) {
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+  res.send(req.file.originalname);
+})
+
+app.listen(3000);
